fd9cc7a0060f312971a5f751758635f76ae61fe2
jack-fortnam <jack@fortnam.uk>
2026-04-15 15:25:28 +0100

    Added first file utility

---
+++ file/file.go
@@ -0,0 +1,23 @@
+package file
+
+import (
+	"bufio"
+	"os"
+)
+
+func Readlines(path string) (lines []string, err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err = scanner.Err(); err != nil {
+		return
+	}
+	return
+}
